b99643bd2225713e74c8b7717a7bf98b5fe5e67a
Louis Klein <louisklein71@gmail.com>
2023-06-12 19:42:18 -0700

    test

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_squared = [int * int for int in int_list]
+    return int_squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(101):
+        if i == 0 :
+            continue
+        if i %3 == 0 and i %5 == 0:
+            # i +=1
+            print("FizzBuzz") 
+        elif i %3 == 0:
+            # i += 1
+            print("Fizz") 
+        elif i%5 == 0:
+            # i += 1
+            print("Buzz") 
+        else :
+            print(i) 
